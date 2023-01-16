318b6214ab04ec50a482d0a0de949262ca98e96d
Maksim Sokolov <vonrut@gmail.com>
2023-01-16 16:44:54 +0300

    first1

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,1 @@
-import express, { NextFunction, Request, Response } from 'express';
-import mongoose from 'mongoose';
-import userRouter from '../routes/users';
-import { IGetUserRequest } from '../models/user';
-
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/mestodb');
-
-const { PORT = 3000 } = process.env;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use('/users', userRouter);
-// app.use('/cards', card);
-
-app.use((req: IGetUserRequest, res: Response, next: NextFunction) => {
-  req!.user = {
-    _id: '63c41e7322eab1089c180c90',
-  };
-  next();
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT} 111`);
-});
+import express, { NextFunction, Request, Response } from 'express';import mongoose from 'mongoose';import userRouter from './routes/users';import { IGetUserRequest } from './models/user';mongoose.set('strictQuery', false);mongoose.connect('mongodb://localhost:27017/mestodb');const { PORT = 3000 } = process.env;const app = express();app.use(express.urlencoded({ extended: false }));app.use(express.json());app.use('/users', userRouter);// app.use('/cards', card);app.use((req: IGetUserRequest, res: Response, next: NextFunction) => {  req!.user = {    _id: '63c41e7322eab1089c180c90',  };  next();});app.listen(PORT, () => {  console.log(`App listening on port ${PORT} 111`);});
